17e1394553f036953d3b59a2193a533abfbc7ebd
WHollid <whollid0@email.cpcc.edu>
2023-09-11 20:58:30 -0400

    Added code for Policy class

---
+++ Policy.java
@@ -0,0 +1,121 @@
+
+public class Policy
+{
+      private int policyNum;
+      private String name;
+      private String firstName;
+      private String lastName;
+      private int holderAge;
+      private boolean smokingStatus;
+      private double holderHeight;
+      private double holderWeight;
+
+      //no arg constructor that initializes
+      public Policy()
+      {
+         policyNum = 0; //variables initialized with default value
+         name = "";
+         firstName = "";
+         lastName = "";
+         holderAge = 0;
+         smokingStatus = false;
+         holderHeight = 0;
+         holderWeight = 0;
+      }
+
+      //constructor with arguments that assigns variables to apporpriate fields
+      public Policy(int numPolicy, String names, String nameFirst, String nameLast, int age, boolean smokeStatus, double height, double weight)
+      {
+         policyNum = numPolicy;
+         name = names;
+         firstName = nameFirst;
+         lastName = nameLast;
+         holderAge = age;
+         smokingStatus = smokeStatus;
+         holderHeight = height;
+         holderWeight = weight;
+      }
+      
+     //mutator setter fields     
+     public void setPolicy(int numPolicy)
+     {
+        policyNum = numPolicy;
+     }
+      
+     public void setname(String names)
+     {
+        name = names;
+     }
+      
+     public void setFirstName(String nameFirst)
+     {
+        firstName = nameFirst;
+     }
+      
+     public void setLastName(String nameLast)
+     {
+        lastName = nameLast;
+     }
+      
+     public void setAge(int age)
+     {
+        holderAge = age;
+     }
+     public void setSmokingStatus(boolean smokeStatus)
+     {
+        smokingStatus = smokeStatus;
+     }
+     public void setHeight(double height)
+     {
+        holderHeight = height;
+     }
+      
+     public void setWeight(double weight)
+     {
+        holderWeight = weight;
+     }
+      
+      //Accessor methods returns the policy object's values 
+      public int getPolicyNum()
+      {  
+         return policyNum;
+      }
+      public String getName(){
+         return name;
+      }
+      public String getFirstName(){
+         return firstName;
+      }
+      public String getLastName(){
+         return lastName;
+      }
+      public int getAge(){
+         return holderAge;
+      }
+      public boolean getSmokingStatus(){
+         return smokingStatus;
+      }
+      public double getHeight(){
+         return holderHeight;
+      }
+      public double getWeight(){
+         return holderWeight;
+      }
+
+      public double getBMI(){
+         return (holderWeight * 703)/(holderHeight*holderHeight);
+      }
+      
+      public double insuranceCalculator(double BMI){
+         double policyFee = 600;
+         if(holderAge > 50){
+            policyFee += 75;
+         }
+         if(smokingStatus == true){
+            policyFee += 100;
+         }
+         if(BMI > 35){
+            policyFee += (BMI - 35)*20;
+         }
+         return policyFee;}
+}
--- Project_Wyatt_Holliday.java
+++ Project_Wyatt_Holliday.java
@@ -4,6 +4,20 @@ public class Project_Wyatt_Holliday
 {
    public static void main(String[] args)
       {
+         int input;
+         String name;
+         boolean status;
+         double numbers;
+         double BMI;
          
+         input = JOptionPane.showInputDialog(
+                  "What is your provider's name?");
+         name = JOptionPane.showInputDialog(
+                  "What is your provider's name?");
+         
+         Policy account = new Policy();
+         
+         
+         BMI = account.getBMI();
       }
 }
